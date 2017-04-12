9ad3d9bc8bb6547029b3efa77eb93e609cc1a27e
Max Franz <maxkfranz@gmail.com>
2017-04-12 14:45:16 -0400

    Use same colours as default cy stylesheet

--- cytoscape-cxtmenu.js
+++ cytoscape-cxtmenu.js
@@ -37,7 +37,7 @@ SOFTWARE.
       */
     ], // function( ele ){ return [ /*...*/ ] }, // example function for commands
     fillColor: 'rgba(0, 0, 0, 0.75)', // the background colour of the menu
-    activeFillColor: 'rgba(92, 194, 237, 0.75)', // the colour used to indicate the selected command
+    activeFillColor: 'rgba(1, 105, 217, 0.75)', // the colour used to indicate the selected command
     activePadding: 20, // additional size in pixels for the active command
     indicatorSize: 24, // the size in pixels of the pointer to the active command
     separatorWidth: 3, // the empty spacing in pixels between successive commands
@@ -46,7 +46,7 @@ SOFTWARE.
     maxSpotlightRadius: 38, // the maximum radius in pixels of the spotlight
     openMenuEvents: 'cxttapstart taphold', // space-separated cytoscape events that will open the menu; only `cxttapstart` and/or `taphold` work here
     itemColor: 'white', // the colour of text in the command's content
-    itemTextShadowColor: 'black', // the text shadow colour of the command's content
+    itemTextShadowColor: 'transparent', // the text shadow colour of the command's content
     zIndex: 9999, // the z-index of the ui div
     atMouse: false // draw menu at mouse position
   };
